5989dc6363c86571ba15fd8687fb66878faa8a9a
waliul <adnancse107@gmail.com>
2014-09-11 16:11:51 +0600

    adding dynamic secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,21 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-CarRental::Application.config.secret_key_base = '89c30349e3c40cda6a763139fb9b5aa6daee2a96c72863beb675f8826aba64f44ad14be23e940af85780924c7eed0f3e71d5e145b130fd0c71e755f70eed97f7'
+#CarRental::Application.config.secret_key_base = '89c30349e3c40cda6a763139fb9b5aa6daee2a96c72863beb675f8826aba64f44ad14be23e940af85780924c7eed0f3e71d5e145b130fd0c71e755f70eed97f7'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+CarRental::Application.config.secret_key_base = secure_token
